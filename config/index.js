7cedef2438bdf8ce5e52116fc074ae175666eafe
Dean <deanocean5656@gmail.com>
2020-10-28 02:01:15 +0900

    update config/index.js: assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Vue-project/',
     proxyTable: {},
 
     // Various Dev Server settings
